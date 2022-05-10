339a0b5c9c994c8d14fbae829495229cc0a1ecd1
JunhoYoon95 <enffl95@icloud.com>
2022-05-10 16:07:21 +0900

    기본적인 Create 폼을 작성하였고, 아직 기능은 입히지 않은 상태입니다

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+    # 게시글들을 보여야겠지. Article 이라는 변수 안에서 모두 post 라는 변수 안에다가 저장을 한다
+    @post = Article.all
   end
 
   def create
@@ -12,6 +14,7 @@ class HomeController < ApplicationController
   end
 
   def create_form
+    
   end
 
   def update_form
